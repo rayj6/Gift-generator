5f102171d7b147874d03e6f87581b7a5c4caec73
rayj6 <nguyentu5526@gmail.com>
2023-09-01 20:33:46 +0700

    Add check gender function

--- components/GetGender.js
+++ components/GetGender.js
@@ -1,15 +1,59 @@
-import { StyleSheet, Text, View, Dimensions } from "react-native";
-import CustomButton from "./CustomButton";
+import { useState } from "react";
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from "react-native";
 
 const WIDTH = Dimensions.get("screen").width;
 const HEIGHT = Dimensions.get("screen").height;
 
 export default function App() {
+    const [Status, setStatus] = useState("");
+    const [Male, setMale] = useState("#FFFFFF");
+    const [MaleText, setMaleText] = useState("#000000");
+    const [Female, setFemale] = useState("#FFFFFF");
+    const [FemaleText, setFemaleText] = useState("#000000");
+    const [Either, setEither] = useState("#FFFFFF");
+    const [EitherText, setEitherText] = useState("#000000");
+
+    function checkGender(gender) {
+        if (gender === "male") {
+            setMale("#4BAE4F");
+            setMaleText("#FFFFFF");
+            setStatus("Male");
+            setFemale("#FFFFFF");
+            setFemaleText("#000000");
+            setEither("#FFFFFF");
+            setEitherText("#000000");
+        } else if (gender === "female") {
+            setFemale("#4BAE4F");
+            setFemaleText("#FFFFFF");
+            setStatus("Female");
+            setMale("#FFFFFF");
+            setMaleText("#000000");
+            setEither("#FFFFFF");
+            setEitherText("#000000");
+        } else if (gender === "either") {
+            setEither("#4BAE4F");
+            setEitherText("#FFFFFF");
+            setStatus("Either");
+            setFemale("#FFFFFF");
+            setFemaleText("#000000");
+            setMale("#FFFFFF");
+            setMaleText("#000000");
+        }
+    }
+
     return (
         <View style={styles.buttons}>
-            <CustomButton title={"Male"} WIDTH={WIDTH} />
-            <CustomButton title={"Female"} WIDTH={WIDTH} />
-            <CustomButton title={"Either"} WIDTH={WIDTH} />
+            <TouchableOpacity style={[styles.check, { backgroundColor: Male }]} onPress={() => checkGender("male")}>
+                <Text style={{ fontSize: WIDTH / 25, color: MaleText }}>Male</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={[styles.check, { backgroundColor: Female }]} onPress={() => checkGender("female")}>
+                <Text style={{ fontSize: WIDTH / 25, color: FemaleText }}>Female</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={[styles.check, { backgroundColor: Either }]} onPress={() => checkGender("either")}>
+                <Text style={{ fontSize: WIDTH / 25, color: EitherText }}>Either</Text>
+            </TouchableOpacity>
         </View>
     );
 }
@@ -23,5 +67,20 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-around",
+    },
+
+    check: {
+        width: WIDTH / 4,
+        height: WIDTH / 8,
+
+        shadowColor: "rgba(0, 0, 0, 0.35)",
+        shadowOffset: { width: 0, height: 5 },
+        shadowOpacity: 1,
+        shadowRadius: 20,
+        padding: 16,
+        borderRadius: WIDTH / 30,
+
+        alignItems: "center",
+        justifyContent: "center",
     },
 });
